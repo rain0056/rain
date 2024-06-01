d8e524fc6a8bbf602a96561ea08f035922e2f125
thunder_7008 <100605370+Thunder7008@users.noreply.github.com>
2024-06-01 11:50:50 +0530

    Create password.js

---
+++ password.js
@@ -0,0 +1,11 @@
+var password;
+var pass1="pass";
+
+password=prompt("Hello there!, enter the password to continue");
+
+if(password==pass1)
+    alert("Nice!, hope so you like it...");
+else
+{
+    window.location="index_2.html";
+}
